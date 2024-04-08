c1af8cb85d9660640defae4ef1f5818a2109c8ff
cwilkinson <cwilkinson@lbl.gov>
2024-04-07 22:23:37 -0700

    Example NEUT generation script

---
+++ interactive/NEUT_MINERvA_example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## The output file name
+OUTFILE=NEUT570_MINERvA_LE_FHC_numu.root
+
+## The file that sets physics parameters for the NEUT simulation
+INCARD=${PWD}/MC_inputs/MINERvA_LE_numu_NEUT.card
+
+## This is NEUT's event generation application
+echo "Starting neutroot2..."
+neutroot2 ${INCARD} ${OUTFILE} &> /dev/null
+
+## Only one NUISANCE step is required for NEUT
+echo "Creating NUISANCE flat trees"
+nuisflat -f GenericVectors -i NEUT:${OUTFILE} -o ${OUTFILE/.root/_NUISFLAT.root}
