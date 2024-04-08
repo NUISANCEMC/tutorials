95757094c1c3b99e10a0a345e9183e2547919c67
cwilkinson <cwilkinson@lbl.gov>
2024-04-07 20:21:11 -0700

    Example NuWro generation script

---
+++ interactive/NUWRO_MINERvA_example.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## The output file name
+OUTFILE=NUWRO_LFGRPA_MINERvA_LE_FHC_numu.root
+
+## The file that sets all physics parameters for the nuwro simulations
+INCARD=${PWD}/MC_inputs/MINERvA_LE_numu_NUWRO_LFGRPA.params
+
+## Flux information required for normalization in the NUISANCE step
+## (this information is passed to nuwro through $INCARD)
+NU_PDG=14
+FLUX_FILE=/opt/nuisance/data/flux/minerva_le_flux.root
+FLUX_HIST=numu_fhc
+
+## This is nuwro's event generation application
+echo "Starting nuwro..."
+nuwro -i ${INCARD} -o ${OUTFILE}
+
+## The next two steps use NUISANCE applications
+echo "Starting PrepareNuWroEvents..."
+PrepareNuWroEvents ${OUTFILE} -F ${FLUX_FILE},${FLUX_HIST},${NU_PDG}
+
+echo "Creating NUISANCE flat trees"
+nuisflat -f GenericVectors -i NuWro:${OUTFILE} -o ${OUTFILE/.root/_NUISFLAT.root}
